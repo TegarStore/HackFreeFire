efc04cac89eb850c1f027c574f460e54387b14c6
TegarStore <88363222+TegarStore@users.noreply.github.com>
2021-08-03 11:53:39 +0700

    Add files via upload

---
+++ cobadulu.sh
@@ -0,0 +1,14 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+#TOOL: HACK AKUN GAME FREE FIRE
+#LANGUAGE: Bash
+#Author: TegarStore
+
+echo "[~] Selamat Datang DiTool Tegar Store [~]"
+echo "[~] Gunakan Dengan Bijak [~]"
+echo "[~] WA ADMIN: 085893093052 [~]"
+
+read -p "Masukan ID :" pilih
+
+case $pilih in
+*)
